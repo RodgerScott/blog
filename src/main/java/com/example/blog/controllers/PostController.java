e88c28eeb21baf9cab903c52920457746b030a79
RodgerScott <rodger.scott@gmail.com>
2018-04-17 14:47:22 -0500

    completed basic exercises

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -1,21 +1,47 @@
 package com.example.blog.controllers;
 
+import com.example.blog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/posts")
-    @ResponseBody
-    public String postsIndex() {
-        return ("posts index page");
+    @GetMapping("posts/index")
+    public String postsIndex(Model model) {
+        List<Post> posts = new ArrayList<>();
+
+        Post post1 = new Post();
+            post1.setTitle("First Test Post");
+            post1.setBody("Sample body");
+
+        Post post2 = new Post();
+            post2.setTitle("Second Test Post");
+            post2.setBody("Sample body");
+
+            posts.add(post1);
+            posts.add(post2);
+
+            model.addAttribute("posts", posts);
+
+
+        return "posts/index";
     }
 
-    @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String indPostView(@PathVariable int id) {
-        return ("view an individual post: " + id);
+    @GetMapping("posts/show")
+    public String indPostView(Model model) {
+
+        Post post1 = new Post();
+        post1.setTitle("First Test Post");
+        post1.setBody("Sample body");
+
+        model.addAttribute("post1", post1);
+
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
