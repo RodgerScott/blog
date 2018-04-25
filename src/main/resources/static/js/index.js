6bf6307e3974bae82ad19af762290e51beff002a
RodgerScott <rodger.scott@gmail.com>
2018-04-24 21:04:46 -0500

    minor tweaks

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     element.innerText = truncateText('.bodyText', 155)
 });
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var element = document.querySelector('.recText');
+    element.innerText = truncateText('.recText', 15)
+});
+
 function truncateText(selector, maxLength) {
     var element = document.querySelector(selector),
         truncated = element.innerText;
